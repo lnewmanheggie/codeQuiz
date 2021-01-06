eb69dd2b8253d988d4e8fab9ebeedfa5ec8435e4
lnewmanheggie <lnewmanheggie@gmail.com>
2021-01-05 22:47:58 -0600

    started working on the javascript logic

---
+++ assets/js/script.js
@@ -0,0 +1,122 @@
+let questionsAnswers = [
+
+    {
+        question: "a question here",
+        choice0: "choice1",
+        choice1: "choice2",
+        choice2: "choice3",
+        choice3: "choice4",
+        answer: "choice2"
+    },
+    {
+        question: "another question here",
+        choice0: "choice21",
+        choice1: "choice22",
+        choice2: "choice23",
+        choice3: "choice24",
+        answer: "choice3"
+    },
+    {
+        question: "a dumb question here",
+        choice0: "choice31",
+        choice1: "choice32",
+        choice2: "choice33",
+        choice3: "choice34",
+        answer: "choice4"
+    },
+    {
+        question: "a really stupid question here",
+        choice0: "choice41",
+        choice1: "choice42",
+        choice2: "choice43",
+        choice3: "choice44",
+        answer: "choice1"
+    }
+
+]
+
+const question = document.body.querySelector("h2");
+const choices = Array.from(document.body.querySelectorAll(".choice"));
+const check = document.body.querySelector("p");
+
+const questionsLeft = [0, 1, 2, 3];
+
+function displayQuestion() {
+
+    //Generate random question with random number
+    let randNum = questionsLeft[Math.floor(Math.random() * questionsLeft.length)];
+    let currentQstn = questionsAnswers[randNum];
+
+    // remove the random number in the questionsLeft array
+    questionsLeft.splice(questionsLeft.indexOf(randNum), 1);
+
+    // Use random number to display question and choices from object
+    question.textContent = currentQstn["question"];
+
+    for (let i = 0; i < choices.length; i++) {
+        choices[i].textContent = currentQstn["choice" + i];
+    }
+
+    let answer = currentQstn["answer"];
+
+    for (let i = 0; i <= choices.length; i++) {
+        if (i < choices.length) {
+            choices[i].addEventListener("click", validateAnswer);
+        } 
+    }
+    
+}
+
+function validateAnswer() {
+    check.textContent = "correct";
+    displayQuestion();
+}
+
+displayQuestion();
+
+
+
+
+// console.log(displayQuestion());
+
+// choices[i].addEventListener("click", displayQuestion);
+
+
+
+    // else {
+    //     window.location.href = "./highscores.html";
+    // }
+
+
+// let i = 0;
+
+// if (i < 4) {
+//     console.log("yes");
+//     choices[i].addEventListener("click", displayQuestion);
+//     i++;
+// } else {
+//     console.log("no")
+//     window.location.href = "./highscores.html";
+// }
+
+
+// do {
+//     choices[i].addEventListener("click", displayQuestion);
+//     i++;
+//     console.log(i);
+// }
+// while (i < choices.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
